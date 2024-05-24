af0bf6cb497ed3c00fb0bb2b74798b4dae686b25
ANG-OUSA <angousa168@gmail.com>
2024-05-24 15:49:50 +0700

    Login

---
+++ lib/view/list_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListTile extends StatefulWidget {
+  const ListTile({super.key});
+
+  @override
+  State<ListTile> createState() => _ListTileState();
+}
+
+class _ListTileState extends State<ListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
